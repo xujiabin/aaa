02dcd90777487657b0e2260e400ed2906fa082f0
xujiabin <xujiabin@boyacai.com>
2013-07-25 14:32:33 +0800

    'fda'

---
+++ gittest/src/gittest/Test2.java
@@ -0,0 +1,11 @@
+package gittest;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*****************feature test *******");
+
+	}
+
+}
